9685cd057bb5671c948917796cdc7c795b8dd687
Ernest <ernest.glukhov@stonybrook.edu>
2023-04-22 16:51:51 -0400

    sdf preprocessing

--- multimer/utils/sdf_preprocessing/run.sh
+++ multimer/utils/sdf_preprocessing/run.sh
@@ -2,11 +2,10 @@
 
 input_dir="/storage/erglukhov/fragments/0000035/sdf_files/"
 output_dir="/storage/erglukhov/fragments/0000035/renumbered_sdf_files/"
-python_script="/path/to/your/python/script.py"
 
 for file in "$input_dir"/*; do
     if [ -f "$file" ]; then
         filename=$(basename "$file")
-        python3 "$python_script" < "$file" > "$output_dir/$filename"
+        python3 renumber_sdf.py -f "$file" -a Cl -o "$output_dir/$filename" > logs
     fi
 done
