daed9ac02e5c6e3887c9920012c3f8989b8eff9c
Ernest <ernest.glukhov@stonybrook.edu>
2023-05-08 16:22:50 -0400

    fix the file

--- multimer/utils/collect_metrics.py
+++ multimer/utils/collect_metrics.py
@@ -1,22 +1,24 @@
 import pandas as pd
 
-init_dir = '/projectnb2/sc3dm/eglukhov/compress/output/train_18'
-version = 'val_3_10_resycles'
-output_csv = f'{init_dir}_{version}.csv'
+if __name__ == '__main__':
 
-# Initialize an empty DataFrame to store the merged data
-merged_df = pd.DataFrame()
+    init_dir = '/projectnb2/sc3dm/eglukhov/compress/output/init_5_18'
+    version = 'val_init_5'
+    output_csv = f'{init_dir}_{version}.csv'
 
-# Loop through file names and the corresponding i values
-for i in range(1, 6):
-    # Read the CSV file
+    # Initialize an empty DataFrame to store the merged data
+    merged_df = pd.DataFrame()
 
-    df = pd.read_csv(f'{init_dir}_{i}/{version}/metrics.csv')
+    # Loop through file names and the corresponding i values
+    for i in range(1, 6):
+        # Read the CSV file
 
-    # Add a new column with the value of i
-    df['model'] = i
+        df = pd.read_csv(f'{init_dir}_{i}/{version}/metrics.csv')
 
-    # Merge the current DataFrame with the merged_df
-    merged_df = pd.concat([merged_df, df], ignore_index=True)
+        # Add a new column with the value of i
+        df['model'] = i
 
-merged_df.to_csv(output_csv, index=False)
+        # Merge the current DataFrame with the merged_df
+        merged_df = pd.concat([merged_df, df], ignore_index=True)
+
+    merged_df.to_csv(output_csv, index=False)
