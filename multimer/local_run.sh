55d0b6cdf84b57662b1289cb747b9c64dba71ea1
Ernest <ernest.glukhov@stonybrook.edu>
2023-07-10 13:42:57 +0100

    sh

--- multimer/local_run.sh
+++ multimer/local_run.sh
@@ -1,10 +1,9 @@
-python preprocess.py \
-          --pre_alignment_path /home/kikodze/projects/phospho/datasets/msa/ \
-          --json_data_path /home/kikodze/projects/phospho/datasets/v2/all_samples.json \
-          --preprocessed_data_dir /home/kikodze/projects/phospho/datasets/v2 \
-          --mmcif_dir /home/kikodze/projects/mmcif_files/ \
-          --n_jobs 32
-
+#python preprocess.py \
+#          --pre_alignment_path /home/kikodze/projects/phospho/datasets/msa/ \
+#          --json_data_path /home/kikodze/projects/phospho/datasets/v2/all_samples.json \
+#          --preprocessed_data_dir /home/kikodze/projects/phospho/datasets/v2 \
+#          --mmcif_dir /home/kikodze/projects/mmcif_files/ \
+#          --n_jobs 32
 
 #export CUDA_VISIBLE_DEVICES=0
 #
@@ -40,38 +39,36 @@ python preprocess.py \
 #    --step 'train'
 
 
-#for model_version in {1..5}; do
-#  echo "Starting training model version: $model_version"
-#  run=18;
-#  project_dir="/projectnb2/sc3dm/eglukhov/compress/";
-#  data_dir=$project_dir/datasets/v4;
-#  project="compress_final";
-#  run_name="init_5_${run}_${model_version}";
-#  output_dir=$project_dir/output/$run_name/;
-#  mkdir -p $output_dir;
-#  python train_multi_gpu.py \
-#            --gpus 10 \
-#            --num_nodes 1 \
-#            --wandb_output_dir $output_dir \
-#            --wandb_project $project \
-#            --wandb_name $run_name \
-#            --wandb_id $run_name \
-#            --model_weights_path /projectnb2/sc3dm/eglukhov/af_params/params_model_${model_version}_multimer_v2.npz \
-#            --model_checkpoint_path $output_dir/checkpoints \
-#            --preprocessed_data_dir $project_dir/datasets/v4/npz_data \
-#            --output_data_path $output_dir/val_init_5 \
-#            --train_json_path $data_dir/train.json \
-#            --val_json_path $data_dir/val.json \
-#            --test_mode_name "val_metrics" \
-#            --n_layers_in_lr_group 10 \
-#            --crop_size 384 \
-#            --max_epochs 10 \
-#            --hyperparams_seed $run \
-#            --learning_rate 0.001 \
-#            --accumulate_grad_batches 1 \
-#            --step 'test'
-#  sleep 1
-#
-#done
-#
-#echo "All jobs submitted."
+for model_version in {1..5}; do
+  echo "Starting training model version: $model_version"
+  run=14;
+  project_dir="/home/kikodze/projects/phospho/";
+  data_dir=$project_dir/datasets/v2;
+  project="phospho";
+  run_name="test_${model_version}_${run}";
+  output_dir=$project_dir/output/$run_name/;
+  mkdir -p $output_dir;
+  python train_multi_gpu.py \
+          --gpus 1 \
+          --num_nodes 1 \
+          --wandb_output_dir $output_dir \
+          --wandb_project $project \
+          --wandb_name $run_name \
+          --wandb_id $run_name \
+          --wandb_offline \
+          --model_weights_path /home/kikodze/projects/af_params/params_model_${model_version}_multimer_v2.npz \
+          --model_checkpoint_path $output_dir/checkpoints \
+          --preprocessed_data_dir $data_dir/npz_data \
+          --output_data_path $output_dir/data \
+          --train_json_path $data_dir/train.json \
+          --val_json_path $data_dir/val.json \
+          --test_mode_name "val" \
+          --max_epochs 10 \
+          --hyperparams_seed 0 \
+          --step 'test'
+  sleep 1
+
+done
+
+echo "All jobs submitted."
+
--- run_rsync.sh
+++ run_rsync.sh
@@ -1,6 +1,6 @@
-LOCAL_DIR="/home/kikodze/projects/mhc/trainable_folding"
-REMOTE_HOST="eglukhov@enki.ams.stonybrook.edu"
-TARGET_DIR="/home/eglukhov/projects/mhc"
+LOCAL_DIR="/Users/ernestglukhov/projects/folding/trainable_folding"
+REMOTE_HOST="kikodze@129.49.83.196"
+TARGET_DIR="/home/kikodze/projects/phospho/"
 FROM_TO_ARG="$LOCAL_DIR $REMOTE_HOST:$TARGET_DIR"
 echo "Uploading $FROM_TO_ARG"
 
@@ -8,4 +8,4 @@ COMMON_ARGS="-avz
 --exclude=.idea
 --exclude=.git"
 
-rsync $COMMON_ARGS $FROM_TO_ARG
+rsync -e "ssh -p 0504" $COMMON_ARGS $FROM_TO_ARG
